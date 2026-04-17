ea5bd27310d7db22b9d82b70095f8c938096d643
Abhinay kalal <abhinaykalal019@gmail.com>
2026-04-17 18:59:40 +0530

    Clean up project: Remove unnecessary files and optimize for Netlify deployment

--- temp_fix.js
+++
@@ -1,9 +0,0 @@
-// Fixed Hanning window function
-function applyHanningWindow(frame) {
-  const windowed = new Float32Array(frame.length);
-  for (let i = 0; i < frame.length; i++) {
-    const windowValue = 0.5 * (1 - Math.cos(2 * Math.PI * i / (frame.length - 1)));
-    windowed[i] = frame[i] * windowValue;
-  }
-  return windowed;
-}
